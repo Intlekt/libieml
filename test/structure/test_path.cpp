47e04fec256d2e5593ff26b268f42a7e11decba9
Louis van Beurden <louis.vanbeurden@gmail.com>
2022-07-30 15:59:31 -0400

    Fix a bug on buildFromPath that was not factorizing path with same path node

--- test/structure/test_path.cpp
+++ test/structure/test_path.cpp
@@ -37,7 +37,7 @@ TEST(ieml_structure_test_case, path_serialization) {
         auto path = reg.get_or_create(std::make_shared<WordPathNode>(std::make_shared<CategoryWord>(ieml::testing::parse_script(&sreg, "wa."))));
         auto path1 = reg.get_or_create(std::make_shared<InflectionPathNode>(plr), {path});
         auto path2 = reg.get_or_create(std::make_shared<RoleNumberPathNode>(RoleType::ROOT), {path1});
-        
+
         EXPECT_TRUE(path2->is_valid()) << "Invalid argument should not have been thrown on path.";
         EXPECT_EQ(path2->to_string_path(), R"(/0/~"we."/"wa.")") << "Invalid path serialization";
     }
@@ -69,7 +69,7 @@ TEST(ieml_structure_test_case, path_serialization) {
         EXPECT_TRUE(path4->is_valid()) << "Invalid argument should not have been thrown on path.";
         EXPECT_EQ(path4->to_string_path(), R"(/0/*"wa."/&"E:E:A:."/[0]/"wa.")") << "Invalid path serialization";
     }
-    
+
 }
 
 TEST(ieml_structure_test_case, path_from_string) {
@@ -128,7 +128,7 @@ TEST(ieml_structure_test_case, path_tree_comparison) {
     EXPECT_LE(*tree1, *tree3);
     EXPECT_LE(*tree1, *tree2);
     EXPECT_LT(*tree1, *tree2);
-    
+
     EXPECT_GE(*tree1, *tree3);
     EXPECT_GE(*tree2, *tree1);
     EXPECT_GT(*tree2, *tree1);
@@ -174,12 +174,12 @@ TEST(ieml_structure_test_case, path_tree_building_error) {
     }
     {
 
-        auto a = reg.get_or_create(std::make_shared<RoleNumberPathNode>(RoleType::ROOT), 
+        auto a = reg.get_or_create(std::make_shared<RoleNumberPathNode>(RoleType::ROOT),
             {reg.get_or_create(std::make_shared<AuxiliaryPathNode>(std::make_shared<AuxiliaryWord>(ieml::testing::parse_script(&sreg, "wa."), RoleType::ROOT)))});
 
-        auto b = reg.get_or_create(std::make_shared<RoleNumberPathNode>(RoleType::ROOT), 
+        auto b = reg.get_or_create(std::make_shared<RoleNumberPathNode>(RoleType::ROOT),
             {reg.get_or_create(std::make_shared<WordPathNode>(std::make_shared<CategoryWord>(ieml::testing::parse_script(&sreg, "wa."))))});
-        
+
         EXPECT_THROW(reg.buildFromPaths({a, b}), std::invalid_argument);
     }
 
@@ -210,7 +210,7 @@ TEST(ieml_structure_test_case, path_tree_register) {
         EXPECT_EQ(*container->getChildrenAsVector()[0]->getChildrenAsVector()[0]->getChildrenAsVector()[0]->getChildrenAsVector()[0]->getChildrenAsVector()[0], *included->getChildrenAsVector()[0]->getChildrenAsVector()[0]->getChildrenAsVector()[0]);
         // pointer
         EXPECT_EQ(container->getChildrenAsVector()[0]->getChildrenAsVector()[0]->getChildrenAsVector()[0]->getChildrenAsVector()[0]->getChildrenAsVector()[0], included->getChildrenAsVector()[0]->getChildrenAsVector()[0]->getChildrenAsVector()[0]);
- 
+
         // assert that container['/#/0/#/0'] == included['/#/0']
         // value
         EXPECT_EQ(*container->getChildrenAsVector()[0]->getChildrenAsVector()[0]->getChildrenAsVector()[0]->getChildrenAsVector()[0], *included->getChildrenAsVector()[0]->getChildrenAsVector()[0]);
@@ -235,7 +235,7 @@ TEST(ieml_structure_test_case, path_tree_register) {
 TEST(ieml_structure_test_case, path_tree_invariant) {
     PARSE_NO_ERRORS(R"(@word "wa.". @word "a.". @word "b.". @node en:root (0 #"wa."). @node en:node0 (0 #"wa.", 1 #"a."). @node en:node1 (0 #"wa.", 1 #"b.").)");
     auto context = parser.getContext();
-    
+
     auto root = context->getCategoryRegister().resolve_category(LanguageString(LanguageType::EN, "root"));
     auto node0 = context->getCategoryRegister().resolve_category(LanguageString(LanguageType::EN, "node0"));
     auto node1 = context->getCategoryRegister().resolve_category(LanguageString(LanguageType::EN, "node1"));
@@ -246,7 +246,7 @@ TEST(ieml_structure_test_case, path_tree_invariant) {
 
     auto reg = context->getPathTreeRegister();
 
-    auto paradigm = reg.get_or_create(std::make_shared<ParadigmPathNode>(), 
+    auto paradigm = reg.get_or_create(std::make_shared<ParadigmPathNode>(),
             {reg.get_or_create(std::make_shared<ParadigmIndexPathNode>(0), {node0}),
              reg.get_or_create(std::make_shared<ParadigmIndexPathNode>(1), {node1})});
 
@@ -260,7 +260,7 @@ TEST(ieml_structure_test_case, path_tree_invariant) {
     ASSERT_NE(invariant, nullptr);
 
     EXPECT_EQ(invariant, root);
-}   
+}
 
 TEST(ieml_structure_test_case, path_prefix) {
     ieml::parser::IEMLParserErrorListener error_listener;
@@ -270,7 +270,7 @@ TEST(ieml_structure_test_case, path_prefix) {
     wregister.declare_script(script, WordType::CATEGORY);
     wregister.define_word(std::make_shared<CategoryWord>(script));
 
-    auto p0 = ieml::parser::parsePath(ctx, R"(/#/0/"wa.")", true);   
+    auto p0 = ieml::parser::parsePath(ctx, R"(/#/0/"wa.")", true);
     auto p1 = ieml::parser::parsePath(ctx, R"(/#/1/"wa.")", true);
 
     ASSERT_NE(p0, nullptr);
@@ -278,10 +278,34 @@ TEST(ieml_structure_test_case, path_prefix) {
 
     EXPECT_FALSE(p0->is_prefix(p1));
 
-    p0 = ieml::parser::parsePath(ctx, R"(/#/0)", true);   
+    p0 = ieml::parser::parsePath(ctx, R"(/#/0)", true);
     p1 = ieml::parser::parsePath(ctx, R"(/#/1/"wa.")", true);
 
     EXPECT_FALSE(p0->is_prefix(p1));
+}
+
+TEST(ieml_structure_test_case, path_factorize_sub_phrase) {
+    ieml::parser::IEMLParserErrorListener error_listener;
+    ieml::parser::ParserContextManager ctx(&error_listener);
+    auto& wregister = ctx.getWordRegister();
+
+    auto script = ieml::testing::parse_script(&ctx.getScriptRegister(), "a.");
+    wregister.declare_script(script, WordType::CATEGORY);
+    wregister.define_word(std::make_shared<CategoryWord>(script));
+
+    auto p0 = ieml::parser::parsePath(ctx, R"(/#/0/#/0/"a.")", true);
+    auto p1 = ieml::parser::parsePath(ctx, R"(/#/1/#/0/"a.")", true);
+    auto p2 = ieml::parser::parsePath(ctx, R"(/#/1/#/1/"a.")", true);
+
+    ASSERT_NE(p0, nullptr);
+    ASSERT_NE(p1, nullptr);
+    ASSERT_NE(p2, nullptr);
+
+    PathTree::Register reg;
+
+    auto pt = reg.buildFromPaths({p0, p1, p2});
+
+    ASSERT_EQ(pt->getChildren().size(), 2); // two children of phrase : role 0 and role 1. Role 1 is factorized
 }
 
 TEST(ieml_structure_test_case, path_is_contained) {
@@ -292,7 +316,7 @@ TEST(ieml_structure_test_case, path_is_contained) {
     wregister.declare_script(script, WordType::CATEGORY);
     wregister.define_word(std::make_shared<CategoryWord>(script));
 
-    auto p0 = ieml::parser::parsePath(ctx, R"(/#/0/"wa.")", true);   
+    auto p0 = ieml::parser::parsePath(ctx, R"(/#/0/"wa.")", true);
     auto p1 = ieml::parser::parsePath(ctx, R"(/#/1/"wa.")", true);
     ASSERT_NE(p0, nullptr);
     ASSERT_NE(p1, nullptr);
@@ -300,7 +324,7 @@ TEST(ieml_structure_test_case, path_is_contained) {
     EXPECT_FALSE(p0->is_contained(p1));
 
 
-    p0 = ieml::parser::parsePath(ctx, R"(/#/0)", true);   
+    p0 = ieml::parser::parsePath(ctx, R"(/#/0)", true);
     p1 = ieml::parser::parsePath(ctx, R"(/#/1/"wa.")", true);
 
     EXPECT_FALSE(p0->is_contained(p1));
